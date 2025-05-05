d6999adaad8d75255f6af152aa5d0c85fd9e2e48
arthurruivo485 <arthurruivo485@gamail.com>
2025-05-04 22:38:31 -0300

    [feat] Initial Port configuration

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const server = express();
+const PORT = process.env.PORT || 3000;
+
+server.get("/", (req, res) => {
+  res.status(200).send("Primeira Rota do Backend");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
