7ab804daa2169ce8c68ec42ba18eacc62230f209
LuizFelipeDSF <160753466+LuizFelipeDSF@users.noreply.github.com>
2025-05-27 10:36:11 +0000

    [feat]aula07

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,15 +2,49 @@ import database from "../../db/connetion.js";
 
 export async function findAll() {
     try {
-        const query = "SELECT id, username, email, password from users;";
+        const query = "SELECT id, name, email, password_hash from users;";
         const statement = database.prepare(query);
         const users = statement.all();
         //statement.finalize();
         return users;
     } catch (error) {
         console.log(error);
-        throw new Error("Error fetching users: "+error.message)
-    } finally{
-        database.close();
+        throw new Error("Error fetching users: " + error.message)
+    } 
+}
+
+export async function create(userData) {
+    try {
+        const query = "INSERT INTO users (name, email, password_hash) VALUES (?, ?, ?);";
+        const statement = database.prepare(query);
+        const result = statement.run(userData.name, userData.email, userData.password_hash);
+        return result;
+    } catch (error) {
+        console.log(error);
+        throw new Error("Error creating user: " + error.message);
+    }
+}
+
+export async function remove(id){
+    try {
+        const query = "DELETE FROM users WHERE id = ?;";
+        const statement = database.prepare(query);
+        const result = statement.run(id);
+        return result;
+    } catch (error) {
+        console.log(error);
+        throw new Error("Error deleting user: " + error.message);
+    }
+}
+
+export async function update(id, userData) {
+    try {
+        const query = "UPDATE users SET name = ?, email = ? WHERE id = ?;";
+        const statement = database.prepare(query);
+        const result = statement.run(userData.name, userData.email, id);
+        return result;
+    } catch (error) {
+        console.log(error);
+        throw new Error("Error updating user:" + error.message);
     }
 }
